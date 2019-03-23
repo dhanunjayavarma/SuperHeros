37ec546cd3f3aeb040e297b67a6fb97a2d0d02ce
KasiraDh <dhanunjaya.kasiraju@adp.com>
2019-03-23 20:59:38 +0530

    changed SuperHero accessSpecifier to private

--- SuperHeros/src/smsservice/Translator.java
+++ SuperHeros/src/smsservice/Translator.java
@@ -3,7 +3,7 @@ package smsservice;
 import com.superheroes.SuperHero;
 
 public class Translator {
-	SuperHero superHero;
+	private SuperHero superHero;
 
 	public String translate(String key) {
 		superHero = new SuperHero();
