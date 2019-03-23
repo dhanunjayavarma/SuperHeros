380e5a8b6f8feab8de7e1038d09a1cf791301d07
KasiraDh <dhanunjaya.kasiraju@adp.com>
2019-03-23 22:05:10 +0530

    removed code field and getCode method from SMSService class

--- SuperHeros/src/smsservice/SMSService.java
+++ SuperHeros/src/smsservice/SMSService.java
@@ -1,26 +1,17 @@
 package smsservice;
 
 class SMSService {
-	private String code;
 	private String sms;
 	private String superHeroName;
 	private Translator translator;
 
 	public void readSms(String sms) {
 		this.sms = sms;
-		this.code = getCode(sms);
-
-	}
-
-	public String getCode(String sms) {
-		String[] sArray = sms.split(" ");
-		return sArray[1];
 	}
 
 	public void callSuperHero() {
-		System.out.println(code);
 		translator = new Translator();
-		superHeroName = translator.translate(this.code);
+		superHeroName = translator.translate(sms);
 		System.out.print("calling " + superHeroName + " to Rescue");
 	}
 }
