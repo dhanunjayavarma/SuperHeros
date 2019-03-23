0eeff14adf374509a1000b2116ec70720755830f
KasiraDh <dhanunjaya.kasiraju@adp.com>
2019-03-23 22:04:18 +0530

    fromatted

--- SuperHeros/src/smsservice/Translator.java
+++ SuperHeros/src/smsservice/Translator.java
@@ -5,11 +5,13 @@ import com.superheroes.SuperHero;
 public class Translator {
 	private SuperHero superHero;
 	private String code;
+
 	public String translate(String sms) {
 		superHero = new SuperHero();
-		code=getCode(sms);
+		code = getCode(sms);
 		return superHero.getSuperHero(code);
 	}
+
 	private String getCode(String sms) {
 		String[] sArray = sms.split(" ");
 		return sArray[1];
